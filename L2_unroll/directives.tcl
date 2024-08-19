72437cc45a202c38d9d6516e981f9dd8dfc64fe8
Erinyes <158277210+erinyes-20@users.noreply.github.com>
2024-08-19 23:42:46 +0530

    rtl push

---
+++ L2_unroll/directives.tcl
@@ -0,0 +1,8 @@
+############################################################
+## This file is generated automatically by Vitis HLS.
+## Please DO NOT edit it.
+## Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+## Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+############################################################
+set_directive_top -name FFT "FFT"
+set_directive_pipeline "FFT_stage/FFT_loop2"
---
+++ L2_unroll/script.tcl
@@ -0,0 +1,22 @@
+############################################################
+## This file is generated automatically by Vitis HLS.
+## Please DO NOT edit it.
+## Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+## Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+############################################################
+open_project 32pt_fft
+set_top FFT
+add_files 32_fft/32fft.h
+add_files 32_fft/32fftvalues.h
+add_files 32_fft/fft_staged.cpp
+add_files -tb 32_fft/fft_tb.cpp -cflags "-Wno-unknown-pragmas"
+add_files -tb 32_fft/inp_cpp.txt -cflags "-Wno-unknown-pragmas"
+add_files -tb 32_fft/out_cpp.txt -cflags "-Wno-unknown-pragmas"
+open_solution "L2_pipe" -flow_target vivado
+set_part {xc7z020-clg484-3}
+create_clock -period 10 -name default
+source "./32pt_fft/L2_pipe/directives.tcl"
+csim_design
+csynth_design
+cosim_design
+export_design -format ip_catalog
